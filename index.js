3b1d683ca93218202aff3bb0a65186a30877fb27
Jessica28 <jessieoyaks4j@gmail.com>
2021-06-24 21:31:43 +0100

    working on contact us

--- index.js
+++ index.js
@@ -1,29 +1,52 @@
 const express = require("express");
-const cors = require("cors");
 const path = require("path");
 const churchRoute = require("./routes/app.routes");
+const nodemailer = require("nodemailer");
 
 const app = express();
 
 const port = 8000;
 
+//middleware
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.json());
-
-app.use(cors());
-
 app.use("/", churchRoute);
-
 app.use("/", express.static(path.join(__dirname, "/public")));
+
+app.get("/contact", (req, res, next) => {
+  res.sendFile(__dirname + "./views/contact.ejs");
+});
+
+app.post("/contact", (req, res, next) => {
+  console.log(req.body);
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "jessieoyaks4j@gmail.com",
+      pass: "2852003.",
+    },
+  });
+
+  const mailOptions = {
+    from: req.body.email,
+    to: "jessieoyaks4j@gmail.com",
+    subject: `Message from ${req.body.email}: ${req.body.subject}`,
+    text: req.body.message,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {});
+});
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
+//error message handler
 app.all("*", (req, res, next) => {
   res.send("<h1>404 page not found 😢😢</h1>");
 });
 
+//server
 app.listen(port, () => {
   console.log(`server is listening to port ${port}`);
 });
